d5cfd72832cd78d5a6793bd274dcc3167fd440fc
doa-b <doa.bonifacio@gmail.com>
2019-07-16 17:56:49 +0200

    Halfway chapter 5

--- src/App.js
+++ src/App.js
@@ -43,11 +43,12 @@ class App extends Component {
 
     render() {
         const style = {
-            backgroundColor: 'white',
+            backgroundColor: 'green',
+            color: 'white',
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
-            cursor: 'pointer'
+            cursor: 'pointer',
         }
 
         let persons = null
@@ -65,12 +66,22 @@ class App extends Component {
                     })}
                 </div>
             );
+
+            style.backgroundColor = 'red';
+        }
+
+        const classes = [] //['red', 'bold'].join(' '); red bold valid css class list
+        if (this.state.persons.length <=2) {
+            classes.push('red') // classes = ['red']
+        }
+        if (this.state.persons.length <=1) {
+            classes.push('bold') // classes = ['red, bold']
         }
 
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
-                <p>This is really working!</p>
+                <p className={classes.join(' ')}>This is really working!</p>
                 <button
                     style={style}
                     onClick={this.togglePersonsHandler}>Toggle Persons
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,8 @@ import './Person.css'
 
 // A React component is just a function returning some JSX
 
-const person = (props) => {
+const person = ( props ) => {
+
     return (
         <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
